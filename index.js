1f1e00705425f8b457fe1d81aa25dd08d2877d4b
ADMIN <pt27122000@gmail.com>
2021-05-09 18:54:44 +0700

    build

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const bins = [
     name: "Bin 1",
     location: "25.1346",
     containers: [
-      { id: 1, trash: 50, space: 50 },
-      { id: 2, trash: 40, space: 60 },
-      { id: 3, trash: 80, space: 20 },
+      { id: 1, trash: 50, space: 50, data_timestamp: [65, 60, 100, 65, 54, 86] },
+      { id: 2, trash: 40, space: 60, data_timestamp: [41, 14, 37, 26, 79, 46] },
+      { id: 3, trash: 80, space: 20, data_timestamp: [94, 12, 44, 57, 43, 24] },
     ],
     trashes: [
       ["bottle", 37],
@@ -22,9 +22,9 @@ const bins = [
     name: "Bin 2",
     location: "25.1346",
     containers: [
-      { id: 1, trash: 10, space: 90 },
-      { id: 2, trash: 10, space: 90 },
-      { id: 3, trash: 10, space: 90 },
+      { id: 1, trash: 10, space: 90, data_timestamp: [81, 80, 59, 100, 3, 65] },
+      { id: 2, trash: 10, space: 90, data_timestamp: [60, 2, 24, 26, 1, 32] },
+      { id: 3, trash: 10, space: 90, data_timestamp: [ 38, 59, 67, 20, 16, 44] },
     ],
     trashes: [
       ["bottle", 11],
@@ -37,9 +37,9 @@ const bins = [
     name: "Bin 3",
     location: "25.1346",
     containers: [
-      { id: 1, trash: 100, space: 0 },
-      { id: 2, trash: 0, space: 100 },
-      { id: 3, trash: 80, space: 20 },
+      { id: 1, trash: 100, space: 0, data_timestamp: [ 20, 55, 46, 98, 32, 65] },
+      { id: 2, trash: 0, space: 100, data_timestamp: [ 56, 41, 9, 24, 66, 79] },
+      { id: 3, trash: 80, space: 20, data_timestamp: [ 83, 31, 78, 59, 2, 27] },
     ],
     trashes: [
       ["bottle", 231],
@@ -52,9 +52,9 @@ const bins = [
     name: "Bin 4",
     location: "25.1346",
     containers: [
-      { id: 1, trash: 78, space: 22 },
-      { id: 2, trash: 22, space: 78 },
-      { id: 3, trash: 33, space: 67 },
+      { id: 1, trash: 78, space: 22, data_timestamp: [ 5, 20, 76, 71, 92, 82] },
+      { id: 2, trash: 22, space: 78, data_timestamp: [ 42, 36, 22, 28, 100, 63] },
+      { id: 3, trash: 33, space: 67, data_timestamp: [ 91, 94, 92, 33, 67, 39] },
     ],
     trashes: [
       ["bottle", 41],
@@ -72,10 +72,9 @@ app.listen(PORT);
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.get("/bins", (req, res) => {
   res.send(bins);
 });
-
---
+++ test.js
@@ -0,0 +1,5 @@
+arr = [];
+for (let index = 0; index < 6; index++) {
+    arr.push(Math.floor(Math.random()*101));
+}
+console.log(arr);
